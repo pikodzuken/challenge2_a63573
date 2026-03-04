9ae5daf487ae1ff85ad2ccada56fe966e91a4c01
Pi <ines.f.ribeiro12@gmail.com>
2026-03-04 00:31:01 +0000

    2.2 passo 7

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class DestroyOutBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = -30;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +14,10 @@ public class DestroyOutBounds : MonoBehaviour
     void Update()
     {
         if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
         }

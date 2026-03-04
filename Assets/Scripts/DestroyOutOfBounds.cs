da92b5a59204f420e44a54b8c2644d00afa621e2
Pi <ines.f.ribeiro12@gmail.com>
2026-03-04 00:25:57 +0000

    2.2 passo 6

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class DestroyOutBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

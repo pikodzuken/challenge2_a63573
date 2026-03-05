b5002c9eaca0f17e571235b67ae64c069aebf9ca
Pi <ines.f.ribeiro12@gmail.com>
2026-03-05 20:01:58 +0000

    bonus passo 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,16 @@
+using System.Security;
 using UnityEngine;
 
-public class Player_controler : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
-
+    public float zRangeDown = - 2.0f;
+    public float zRangeUp = 7.0f;
+    
     public GameObject ProjectilePrefab;
-
     void Start()
     {
         
@@ -16,17 +19,26 @@ public class Player_controler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -xRange)
+        if (transform.position.x < - xRange)
         {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(- xRange, transform.position.y, transform.position.z);
         }
-
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if (transform.position.z < zRangeDown)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeDown);
+        }
+        if (transform.position.z > zRangeUp)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeUp);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {

953820c90e5053daee64142b0bc8fc0b7d7f5f8a
Pi <ines.f.ribeiro12@gmail.com>
2026-03-06 01:39:19 +0000

    bonus passo 4

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,27 +5,39 @@ public class DestroyOutOfBounds : MonoBehaviour
     private float topBound = 30;
     private float lowerBound = -10;
     private float sideBound = 25;
+    private GameManager gameManager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //If an object goes past the players view in the game, remove that object
+        // If an object goes past the players view in the game, remove that object
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         }
-        else if (transform.position.z <lowerBound)
+        else if (transform.position.z < lowerBound)
         {
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
-        else if (transform.position.x > sideBound || transform.position.x < -sideBound)
+        else if (transform.position.x > sideBound)
         {
+            gameManager.AddLives(-1);
+            Destroy(gameObject);
+        }
+        else if (transform.position.x < -sideBound)
+        {
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
     }
+
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int score = 0;
+    private int lives = 3;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void AddLives(int value)
+    {
+        lives += value;
+        if (lives <= 0)
+        {
+            Debug.Log("Game Over");
+            lives = 0;
+        }
+        Debug.Log("Lives = " + lives);
+    }
+    public void AddScore(int value)
+    {
+        score += value;
+        Debug.Log("Score = " + score);
+    }
+
+}
